7eaa76fe1be63ca94603fb5bef8f03f317d98531
SuccessByFailure <successbyfailure@gmail.com>
2020-03-24 14:32:43 +0100

    clase para el caudalimetro por pulsos

---
+++ Firmware/aire_apollo/src/FlowSensorInt.cpp
@@ -0,0 +1,57 @@
+#ifndef FLOWIC
+#define FLOWIC
+
+#include "Arduino.h"
+
+//ojo! para que esta clase funcione correctamente hay que llamar a pulse() desde la interrupcion del pin del sensor.
+// Tambien hay que llamar a la funcion update con una interrupcion cada 1ms.
+
+class FlowSensorInt
+{
+  public:
+    FlowSensorInt(uint16_t pulses_per_liter,uint8_t sampling_ms = 10):
+    _pulsesPerLiter(pulses_per_liter), _samplingMS(sampling_ms)
+    {
+
+    }
+
+    void pulse()
+    {
+      _pulsesSinceLastSample++;
+      _pulseCounter++;
+    }
+
+    void update()
+    {
+      if (millis()%_samplingMS)
+      {
+        _instantFlow = _pulsesSinceLastSample / float(_pulsesPerLiter) * 1000 ;
+      }
+    }
+
+    uint16_t getInstantFlow()
+    {
+      return _instantFlow;
+    }
+
+    unsigned long getFlow()
+    {
+      return _pulseCounter / float(_pulsesPerLiter) * 1000;
+    }
+
+    void resetFlow()
+    {
+      _pulseCounter = 0;
+    }
+
+
+
+  protected:
+      uint16_t      _pulsesPerLiter;
+      uint8_t       _samplingMS;
+      uint16_t      _pulsesSinceLastSample;
+      uint16_t      _instantFlow;
+      unsigned long _pulseCounter;
+};
+
+#endif
